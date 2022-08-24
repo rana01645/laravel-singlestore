a350ee2d4dee5bed1d489ff8003dd8b66cbe27e6
rana01645 <rana01645@gmail.com>
2022-08-24 17:46:15 +0600

    update gitignore

--- config/database.php
+++ config/database.php
@@ -35,6 +35,28 @@ return [
 
     'connections' => [
 
+        //same as the mysql one with singlestore driver
+        'singlestore' => [
+            'driver' => 'singlestore',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'unix_socket' => env('DB_SOCKET'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::ATTR_EMULATE_PREPARES => true,
+            ]) : [],
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
